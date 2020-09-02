55a7ac6c754a48f3920e217eea32d851ea38f806
jsealt <jsealt@users.noreply.github.com>
2020-09-02 10:21:19 -0400

    client.js update

--- Hello360/index.js
+++ Hello360/index.js
@@ -1,43 +1,42 @@
-import React from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View,
-} from 'react-360';
+import React from "react";
+import { AppRegistry, StyleSheet, Text, View } from "react-360";
 
-export default class Hello360 extends React.Component {
+export default class app_react360 extends React.Component {
   render() {
     return (
       <View style={styles.panel}>
         <View style={styles.greetingBox}>
-          <Text style={styles.greeting}>
-            Welcome to React 360
+          <Text style={styles.greeting}>"Chase your vision" </Text>
+          <Text style={(styles.greeting, (fontSize = 24))}>
+            Lothian, MD - July 2020{" "}
           </Text>
         </View>
       </View>
     );
   }
-};
+}
 
 const styles = StyleSheet.create({
   panel: {
     // Fill the entire surface
     width: 1000,
     height: 600,
-    backgroundColor: 'rgba(255, 255, 255, 0.4)',
-    justifyContent: 'center',
-    alignItems: 'center',
+    // backgroundColor: "rgba(255, 255, 255, 0.4)",
+    justifyContent: "center",
+    alignItems: "center",
   },
   greetingBox: {
-    padding: 20,
-    backgroundColor: '#000000',
-    borderColor: '#639dda',
+    padding: 40,
+    backgroundColor: "#623cea",
+    borderColor: "#f3f3f3",
     borderWidth: 2,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 50,
   },
   greeting: {
-    fontSize: 30,
+    fontSize: 80,
   },
 });
 
-AppRegistry.registerComponent('Hello360', () => Hello360);
+AppRegistry.registerComponent("app_react360", () => app_react360);
